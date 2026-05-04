3371d5e43ca44eedb7142acb7ddab63b137fdf2a
push-shihab <shihabfamily539@gmail.com>
2026-05-04 23:11:44 +0600

    no solution found

--- src/app/api/auth/[...all]/route.js
+++ src/app/api/auth/[...all]/route.js
@@ -1,6 +1,4 @@
 import { auth } from "@/app/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
-export const dynamic = "force-dynamic";
-export const runtime = "nodejs"; // if needed
 
 export const { POST, GET } = toNextJsHandler(auth);
